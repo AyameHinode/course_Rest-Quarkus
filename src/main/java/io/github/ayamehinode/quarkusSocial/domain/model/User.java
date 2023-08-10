d517dd997dcad9ad2db918e1fd8fd3a566b7486a
Ayame Hinode <ayamehinode@gmail.com>
2023-08-10 20:35:22 -0300

    Lombok library applied

--- src/main/java/io/github/ayamehinode/quarkusSocial/domain/model/User.java
+++ src/main/java/io/github/ayamehinode/quarkusSocial/domain/model/User.java
@@ -3,10 +3,12 @@ package io.github.ayamehinode.quarkusSocial.domain.model;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Objects;
 
 @Entity
+@Data
 @Table(name = "users")
 public class User {
 
@@ -17,42 +19,5 @@ public class User {
     private String name;
     @Column(name = "age")
     private Integer age;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(age, user.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
 
 }
