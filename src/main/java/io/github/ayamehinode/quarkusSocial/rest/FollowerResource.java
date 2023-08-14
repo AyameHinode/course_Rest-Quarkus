79e9847e2c1d7d9593104db8158cd178a5f68d35
Ayame Hinode <ayamehinode@gmail.com>
2023-08-13 21:43:31 -0300

    Put method implemented

--- src/main/java/io/github/ayamehinode/quarkusSocial/rest/FollowerResource.java
+++ src/main/java/io/github/ayamehinode/quarkusSocial/rest/FollowerResource.java
@@ -1,12 +1,13 @@
 package io.github.ayamehinode.quarkusSocial.rest;
 
+import io.github.ayamehinode.quarkusSocial.domain.model.Follower;
 import io.github.ayamehinode.quarkusSocial.domain.repository.FollowerRepository;
 import io.github.ayamehinode.quarkusSocial.domain.repository.UserRepository;
+import io.github.ayamehinode.quarkusSocial.rest.dto.FollowerRequest;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -20,6 +21,23 @@ public class FollowerResource {
     public FollowerResource(FollowerRepository followerRepository, UserRepository userRepository){
         this.userRepository = userRepository;
         this.followerRepository = followerRepository;
+    }
+
+    @PUT
+    public Response followUser(@PathParam("userId")Long userId, FollowerRequest request){
+        var user = userRepository.findById(userId);
+        if(user == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        var follower = userRepository.findById(request.getFollowerId());
+
+        var entity = new Follower();
+        entity.setUser(user);
+        entity.setFollower(follower);
+
+        followerRepository.persist(entity);
+        return Response.status(Response.Status.NO_CONTENT).build();
+
     }
 
 }
