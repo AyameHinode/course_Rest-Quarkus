5c98d7e230d6272a46203087c813907610e53507
Ayame Hinode <ayamehinode@gmail.com>
2023-08-18 01:12:08 -0300

    Validation test created for UserResource

--- src/test/java/io/github/ayamehinode/quarkusSocial/rest/UserResourceTest.java
+++ src/test/java/io/github/ayamehinode/quarkusSocial/rest/UserResourceTest.java
@@ -1,12 +1,16 @@
 package io.github.ayamehinode.quarkusSocial.rest;
 
 import io.github.ayamehinode.quarkusSocial.rest.dto.CreateUserRequest;
+import io.github.ayamehinode.quarkusSocial.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import jakarta.json.bind.JsonbBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
@@ -30,6 +34,30 @@ class UserResourceTest {
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("Should Run an error when Json is not valid")
+    public void createUserValidationErrorTest(){
+        var user = new CreateUserRequest();
+        user.setAge(null);
+        user.setName(null);
+
+        var response =
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(JsonbBuilder.create().toJson(user))
+                    .when()
+                        .post("/users")
+                    .then()
+                        .extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errorsList = response.jsonPath().getList("errors");
+        assertNotNull(errorsList.get(0).get("message"));
+        assertNotNull(errorsList.get(1).get("message"));
     }
 
 }
