fed3230c18c92133bd2b15b3479945033c862ae0
Ayame Hinode <ayamehinode@gmail.com>
2023-08-18 00:18:08 -0300

    Jsonb bug fixed at UserTest

--- src/test/java/io/github/ayamehinode/quarkusSocial/rest/UserResourceTest.java
+++ src/test/java/io/github/ayamehinode/quarkusSocial/rest/UserResourceTest.java
@@ -3,6 +3,8 @@ package io.github.ayamehinode.quarkusSocial.rest;
 import io.github.ayamehinode.quarkusSocial.rest.dto.CreateUserRequest;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import jakarta.json.bind.JsonbBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,13 +21,12 @@ class UserResourceTest {
         user.setName("Ana");
         user.setAge(15);
 
-        var response = given()
-            .contentType(ContentType.JSON)
-            .body(user)
-        .when()
-            .post("/users")
-        .then()
-            .extract().response();
+        Response response;
+        response = given()
+                .contentType(ContentType.JSON).body(JsonbBuilder.create().toJson(user)).when()
+                .post("/users")
+                .then()
+                .extract().response();
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
