dceddec98da6923e26ec3d0963c6895d85d3ac25
Ayame Hinode <ayamehinode@gmail.com>
2023-08-18 23:17:37 -0300

    Test listAllUsers created, all tests working

--- src/test/java/io/github/ayamehinode/quarkusSocial/rest/UserResourceTest.java
+++ src/test/java/io/github/ayamehinode/quarkusSocial/rest/UserResourceTest.java
@@ -2,23 +2,31 @@ package io.github.ayamehinode.quarkusSocial.rest;
 
 import io.github.ayamehinode.quarkusSocial.rest.dto.CreateUserRequest;
 import io.github.ayamehinode.quarkusSocial.rest.dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import jakarta.json.bind.JsonbBuilder;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserResourceTest {
 
+    @TestHTTPResource("/users")
+    URL apiURL;
+
     @Test
+    @Order(1)
     @DisplayName("Should create an user successfully")
     public void createUserTest(){
         var user = new CreateUserRequest();
@@ -28,7 +36,7 @@ class UserResourceTest {
         Response response;
         response = given()
                 .contentType(ContentType.JSON).body(JsonbBuilder.create().toJson(user)).when()
-                .post("/users")
+                .post(apiURL)
                 .then()
                 .extract().response();
 
@@ -37,6 +45,7 @@ class UserResourceTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("Should Run an error when Json is not valid")
     public void createUserValidationErrorTest(){
         var user = new CreateUserRequest();
@@ -48,7 +57,7 @@ class UserResourceTest {
                         .contentType(ContentType.JSON)
                         .body(JsonbBuilder.create().toJson(user))
                     .when()
-                        .post("/users")
+                        .post(apiURL)
                     .then()
                         .extract().response();
 
@@ -58,6 +67,19 @@ class UserResourceTest {
         List<Map<String, String>> errorsList = response.jsonPath().getList("errors");
         assertNotNull(errorsList.get(0).get("message"));
         assertNotNull(errorsList.get(1).get("message"));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Should list all users")
+    public void  listAllUsersTest(){
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(apiURL)
+                .then()
+                .statusCode(200)
+                .body("size()", Matchers.is(1));
     }
 
 }
